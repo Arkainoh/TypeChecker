f546d43e024b22c7e05f508d261fb3eb073469f0
Arkainoh <arkainoh@gmail.com>
2016-12-02 05:25:07 +0900

    Compile

---
+++ PL2016-typechecker/parser.mli
@@ -0,0 +1,49 @@
+type token =
+  | NUM of (int)
+  | TRUE
+  | FALSE
+  | ID of (string)
+  | INT
+  | PLUS
+  | MINUS
+  | STAR
+  | SLASH
+  | EQUAL
+  | EQUALEQUAL
+  | LE
+  | LT
+  | GE
+  | GT
+  | NOT
+  | AND
+  | OR
+  | IF
+  | THEN
+  | ELSE
+  | WHILE
+  | DO
+  | READ
+  | PRINT
+  | SEMICOLON
+  | LET
+  | IN
+  | COMMA
+  | ISZERO
+  | LETREC
+  | PROC
+  | SET
+  | NEWREF
+  | DEREF
+  | SETREF
+  | BEGIN
+  | END
+  | LBRACE
+  | RBRACE
+  | LBLOCK
+  | RBLOCK
+  | LPAREN
+  | RPAREN
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Type.program
